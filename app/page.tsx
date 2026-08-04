b2b263c48268238460604c71153dec79344990fe
nomaan-07 <nomaan07.dev@gmail.com>
2026-08-04 21:54:36 +0330

    feat: add website shared components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import Footer from "@/components/shared/Footer";
+import Navbar from "@/components/shared/Navbar";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main>
-      <Button>برفا موبایل</Button>
-    </main>
+    <>
+      <Navbar />
+      <main>
+        <Button>برفا موبایل</Button>
+      </main>
+      <Footer />
+    </>
   );
 }
