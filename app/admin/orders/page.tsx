0dc07a3a6ca0030a43003ac050bebc6e08815fb0
nomaan-07 <nomaan07.dev@gmail.com>
2026-09-18 03:03:11 +0330

    refactor(admin): remove unnecessary orders wrapper

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,11 +1,15 @@
-import AdminOrders from "@/features/admin/orders/AdminOrders";
+import AdminOrdersTable from "@/features/admin/orders/AdminOrdersTable";
+import AdminOrdersTableControls from "@/features/admin/orders/AdminOrdersTableControls";
+import AdminStatusTabs from "@/features/admin/orders/AdminStatusTabs";
 import { Suspense } from "react";
 
 function OrdersPage() {
   // FIXME: Add skeleton
   return (
     <Suspense fallback={null}>
-      <AdminOrders />
+      <AdminOrdersTableControls />
+      <AdminStatusTabs />
+      <AdminOrdersTable />
     </Suspense>
   );
 }
