c760399f2a3d360ba835b760996a6ca8fd08d752
nomaan-07 <nomaan07.dev@gmail.com>
2026-09-19 12:30:05 +0330

    style(admin): update customers table controls layout

--- features/admin/customers/CustomersTableControls/index.tsx
+++ features/admin/customers/CustomersTableControls/index.tsx
@@ -4,13 +4,13 @@ import CustomersTableSort from "./components/CustomersTableSort";
 
 function CustomersTableControls() {
   return (
-    <>
-      <div className="flex flex-col justify-between gap-6 sm:flex-row sm:items-center">
-        <AdminSearch className="max-sm:order-2" />
-        <AddCustomerButton />
+    <div className="flex flex-col justify-between gap-6 sm:flex-row sm:items-center">
+      <div className="flex flex-col flex-wrap gap-6 max-sm:order-2 sm:flex-row sm:items-center sm:gap-2">
+        <CustomersTableSort />
+        <AdminSearch />
       </div>
-      <CustomersTableSort />
-    </>
+      <AddCustomerButton />
+    </div>
   );
 }
 
